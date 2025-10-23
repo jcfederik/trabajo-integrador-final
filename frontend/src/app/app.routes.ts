66558d767246aeef7fc17859d535216e1070c058
juandefuego <juanifederik@gmail.com>
2025-10-23 20:44:29 -0300

    feat: agregar componente de dashboard con tarjetas de navegación y estilos de Bootstrap

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes, provideRouter } from '@angular/router'; 
 import { Login } from './components/login/login';
- 
+import { DashboardComponent } from './components/dashboard/dashboard';
 export const routes: Routes = [  
-{ path: 'login', component: Login }, 
+{ path: 'login', component: Login },
+{ path: 'dashboard', component: DashboardComponent },
+
 ]; 
