7e4b68bf5a02dfbeb421063c371f3f0c8251bce2
Fátima Passarella <fatypassarella.1409@gmail.com>
2025-10-24 16:37:54 -0300

    sidebar

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBar } from './components/nav-bar/nav-bar';
+import { SidebarComponent } from "./sidebar/sidebar";
 
 @Component({
   selector: 'app-root',
   standalone: true,   
-  imports: [RouterOutlet, NavBar],
+  imports: [RouterOutlet, NavBar, SidebarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
